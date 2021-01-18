71fd402539f9454af7dfc13ebbccf5869a936a08
Danny393 <danielacosta393@gmail.com>
2021-01-17 23:11:26 -0500

    Movie API request handled and info is being received

--- app/src/main/java/com/example/flixster/MainActivity.java
+++ app/src/main/java/com/example/flixster/MainActivity.java
@@ -3,12 +3,66 @@ package com.example.flixster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.example.flixster.models.Movie;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Headers;
 
 public class MainActivity extends AppCompatActivity {
+
+    final String MOVIE_DB_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    final String TAG = "MainActivity";
+    List<Movie> movies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(MOVIE_DB_URL, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int i, Headers headers, JSON json) {
+                Log.d(TAG, "onSuccess");
+                JSONObject jsonObject = json.jsonObject;
+
+                try {
+                    JSONArray resultsArray = jsonObject.getJSONArray("results");
+                    Log.i(TAG, "Results: " + resultsArray.toString());
+                    movies = fromJsonArray(resultsArray);
+                    Log.i(TAG, "Movies: " + movies.size());
+                }
+                catch (JSONException e) {
+                    Log.e(TAG,"JSON exception", e);
+                }
+            }
+
+            @Override
+            public void onFailure(int i, Headers headers, String s, Throwable throwable) {
+                Log.d(TAG, "onFailure");
+            }
+        });
+    }
+
+    public List<Movie> fromJsonArray(JSONArray movieArray) throws JSONException
+    {
+        List<Movie> movies = new ArrayList<>();
+
+        for(int i = 0; i < movieArray.length(); i++)
+        {
+            movies.add(new Movie(movieArray.getJSONObject(i)));
+        }
+
+        return movies;
     }
 }
---
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -0,0 +1,46 @@
+package com.example.flixster.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie {
+    String title;
+    String posterIMG;
+    String summary;
+
+    public Movie(JSONObject movieObject) throws JSONException
+    {
+        title = movieObject.getString("title");
+        posterIMG = movieObject.getString("poster_path");
+        summary = movieObject.getString("overview");
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getPosterIMG() {
+        return "https://image.tmdb.org/t/p/w342/" + posterIMG;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    //this felt out of place here, moved to main activity for now unless problems arise
+    /*public static List<Movie> fromJsonArray(JSONArray movieArray) throws JSONException
+    {
+        List<Movie> movies = new ArrayList<>();
+
+        for(int i = 0; i < movieArray.length(); i++)
+        {
+            movies.add(new Movie(movieArray.getJSONObject(i)));
+        }
+
+        return movies;
+    }*/
+}
